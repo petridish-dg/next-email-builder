3197cdba677d0f1fed81db4fc9bcbdc44e5a2d14
petridish-dg <mail@peterdg.com>
2023-10-25 16:15:43 -0500

    trying jotai

--- components/main/right/PreviewEmail.js
+++ components/main/right/PreviewEmail.js
@@ -1,9 +1,20 @@
+import { useAtom } from "jotai";
 import styles from "./PreviewEmail.module.css";
+import { selectedTwoUpsAtom } from "@/data/preview-store";
 
 export default function PreviewEmail() {
+    const [selectedTwoUps] = useAtom(selectedTwoUpsAtom);
+
     return (
-        <div>
+        <>
             <div>Preview email here</div>
-        </div>
+            <div className={styles["preview-container"]}>
+                {selectedTwoUps.map((component, index) => (
+                    <div key={index}>
+                        {/* Render each component here */}
+                    </div>
+                ))}
+            </div>
+        </>
     );
 }
---
+++ data/preview-store.js
@@ -0,0 +1,44 @@
+import { atom, useAtom } from 'jotai';
+
+// Create an atom to hold the selected two ups array
+export const selectedTwoUpsAtom = atom([]);
+
+// Create a function to add a two up to the selected array
+export const addSelectedTwoUp = atom(null, (get, set, twoUp) => {
+    const selectedTwoUps = get(selectedTwoUpsAtom);
+    set(selectedTwoUpsAtom, [...selectedTwoUps, twoUp]);
+});
+
+// Create a function to remove a two up from the selected array
+export const removeSelectedTwoUp = atom(null, (get, set, twoUp) => {
+    const selectedTwoUps = get(selectedTwoUpsAtom);
+    set(selectedTwoUpsAtom, selectedTwoUps.filter((item) => item.id !== twoUp.id));
+});
+
+// Example usage in a component
+function MyComponent() {
+    const [selectedTwoUps, setSelectedTwoUps] = useAtom(selectedTwoUpsAtom);
+    const addTwoUp = useAtom(addSelectedTwoUp)[1];
+    const removeTwoUp = useAtom(removeSelectedTwoUp)[1];
+
+    const handleAddTwoUp = (twoUp) => {
+        addTwoUp(twoUp);
+    };
+
+    const handleRemoveTwoUp = (twoUp) => {
+        removeTwoUp(twoUp);
+    };
+
+    return (
+        <div>
+            {selectedTwoUps.map((twoUp) => (
+                <div key={twoUp.id}>
+                    <p>{twoUp.title}</p>
+                    <button onClick={() => handleRemoveTwoUp(twoUp)}>Remove</button>
+                </div>
+            ))}
+            <button onClick={() => handleAddTwoUp({ id: 1, title: 'Two Up 1' })}>Add Two Up 1</button>
+            <button onClick={() => handleAddTwoUp({ id: 2, title: 'Two Up 2' })}>Add Two Up 2</button>
+        </div>
+    );
+}
